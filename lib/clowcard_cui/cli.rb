2f845171a9e0c7b50025fdadae3901d53c60d46d
kuromike0629 <kuromike0629@gmail.com>
2018-01-16 14:23:27 +0900

    [add] change wait time.

--- lib/clowcard_cui/cli.rb
+++ lib/clowcard_cui/cli.rb
@@ -23,7 +23,7 @@ module ClowCardCui
         @pol.add_domain(@new_domain_name)
         @pol.set_profile(@new_domain_name,1)
         @pol.apply
-        sleep(3)
+        sleep(1)
         #実行
         @container = @malware_image.run('/'+File.basename(malware_path))
         sleep(seconds.to_i)
